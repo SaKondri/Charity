25ae010c66e70cdb79c812536e5b15c62c204920
sakondri <saeid.kondri@gmail.com>
2013-03-03 11:39:20 +0330

    O

--- Charity/src/com/friends/charity/view/template/general/GeneralTempPage.java
+++ Charity/src/com/friends/charity/view/template/general/GeneralTempPage.java
@@ -2,9 +2,7 @@ package com.friends.charity.view.template.general;
 
 import java.io.Serializable;
 
-import javax.enterprise.context.RequestScoped;
 import javax.enterprise.context.SessionScoped;
-import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.inject.Inject;
@@ -14,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import com.friends.charity.view.template.general.pic.GalleriaPage;
 
 @Named
-@RequestScoped
+@SessionScoped
 public class GeneralTempPage implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private String changePage = "/WEB-INF/template/general/pic/galleria.xhtml";
@@ -41,7 +39,7 @@ public class GeneralTempPage implements Serializable {
 		String page = ((HttpServletRequest) FacesContext.getCurrentInstance()
 				.getExternalContext().getRequest()).getParameter("madadjo");
 		setChangePage(page);
-		System.out.println(getChangePage());
+
 	}
 
 }
