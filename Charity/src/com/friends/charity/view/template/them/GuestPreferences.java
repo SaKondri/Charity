6f675dbb7ce3b9e1186fdd225e2bf0fe0bc5fcb1
sina-maleki <sina-maleki@sina-maleki>
2013-03-04 20:57:03 +0330

    s

--- Charity/src/com/friends/charity/view/template/them/GuestPreferences.java
+++ Charity/src/com/friends/charity/view/template/them/GuestPreferences.java
@@ -2,30 +2,31 @@ package com.friends.charity.view.template.them;
 
 import java.io.Serializable;
 
-import com.sun.xml.xsom.impl.scd.Iterators.Map;
-
-
 import javax.enterprise.context.SessionScoped;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
+
+import com.sun.xml.xsom.impl.scd.Iterators.Map;
+
 @Named
 @SessionScoped
 public class GuestPreferences implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String theme = "aristo-rtl"; // default
 
-        private String theme = "aristo-rtl"; //default
+	@SuppressWarnings("unchecked")
+	public String getTheme() {
+		Map<String, String> params = (Map<String, String>) FacesContext
+				.getCurrentInstance().getExternalContext()
+				.getRequestParameterMap();
+		if (((java.util.Map<String, String>) params).containsKey("theme")) {
+			theme = ((java.util.Map<String, String>) params).get("theme");
+		}
 
-        public String getTheme() {
-                Map<String, String> params = (Map<String, String>) FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
-                if(((java.util.Map<String, String>) params).containsKey("theme")) {
-                        theme = ((java.util.Map<String, String>) params).get("theme");
-                }
-               
-                return theme;
-        }
+		return theme;
+	}
 
-        public void setTheme(String theme) {
-                this.theme = theme;
-        }
+	public void setTheme(String theme) {
+		this.theme = theme;
+	}
 }
