72ffa38d5a94e4ba187c53f363a415a6bfd92068
sina-maleki <sina-maleki@sina-maleki>
2013-04-07 16:41:49 +0430

    s

---
+++ Charity/src/com/friends/charity/model/Login.java
@@ -0,0 +1,45 @@
+package com.friends.charity.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "LOGIN")
+@NamedQueries({ @NamedQuery(name = "selectUsernamePassword", query = "select u from Login  u where u.usernamePassword.username=:username and u.usernamePassword.password=:password"),
+	@NamedQuery(name="all",query="select u from Login u")
+
+})
+public class Login extends BaseEntity {
+	@OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	private User user;
+	@OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	private UsernamePassword usernamePassword;
+
+	public User getUser() {
+		if (user == null) {
+			user = new User();
+		}
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public UsernamePassword getUsernamePassword() {
+		if (usernamePassword == null) {
+			usernamePassword = new UsernamePassword();
+		}
+		return usernamePassword;
+	}
+
+	public void setUsernamePassword(UsernamePassword usernamePassword) {
+		this.usernamePassword = usernamePassword;
+	}
+
+}
